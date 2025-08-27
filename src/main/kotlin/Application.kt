ed18fac30390f854f6faeff6f7b5def100768ec8
SergeyBoykov <sutemi67@gmail.com>
2025-08-27 12:08:09 +0300

    patch

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -9,14 +9,17 @@ import io.ktor.server.netty.*
 import org.jetbrains.exposed.v1.jdbc.Database
 
 fun main(args: Array<String>) {
-
-    with(ServerPasswords) {
-        Database.connect(
-            url = URL,
-            driver = DRIVER,
-            user = USER,
-            password = PASSWORD
-        )
+    try {
+        with(ServerPasswords) {
+            Database.connect(
+                url = URL,
+                driver = DRIVER,
+                user = USER,
+                password = PASSWORD
+            )
+        }
+    } catch (e: Exception) {
+        println("cant connect to database -> ${e.message}")
     }
 
     embeddedServer(
